dbf547d635c733eff7d2e2c62cba007d85130552
Oliver Lang <lang.oliver@gmail.com>
2020-06-04 10:26:21 +0200

    4.5: helper functions and unit tests, step3

--- part4/blog/src/tests/list_helper.test.js
+++ part4/blog/src/tests/list_helper.test.js
@@ -17,9 +17,38 @@ describe('total likes', () => {
         __v: 0
       }
     ]
+    
   
     test('when list has only one blog equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
       expect(result).toBe(5)
+    })
+  })
+
+  describe('favorite blog', () => {
+    const listWithTwoBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f9',
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 12,
+            __v: 0
+          }
+      ]
+    test('two blog returns blog with most likes', () => {
+        const result = listHelper.favoriteBlog(listWithTwoBlogs)
+        expect(result).toEqual({ 
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12})
     })
   })
--- part4/blog/src/utils/list_helper.js
+++ part4/blog/src/utils/list_helper.js
@@ -9,7 +9,19 @@ const totalLikes = (blogs) =>{
         return prev + cur.likes
     }, 0)
 }
-  
+
+const favoriteBlog = (blogs) => {
+    const favoriteBlogEntry = blogs.reduce(function(prev, cur) {
+        return (prev.likes > cur.likes) ? prev : cur
+    }, {likes:0})
+    const formattedBlogEntry = new Object() 
+    formattedBlogEntry.title = favoriteBlogEntry.title
+    formattedBlogEntry.author = favoriteBlogEntry.author
+    formattedBlogEntry.likes = favoriteBlogEntry.likes
+    return formattedBlogEntry
+    
+    
+}
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
