a6c52c37f35a0b1c824c8a63589512226f8a5cb2
Oliver Lang <lang.oliver@gmail.com>
2020-06-05 15:20:56 +0200

    4.13 Blog list expansions, step1

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -18,4 +18,14 @@ blogsRouter.get('/', async (request, response) => {
      }      
     })
 
+    blogsRouter.delete('/:id', async (request, response, next) => {
+      try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+      }
+      catch(exception) {
+        next(exception)
+      }
+    })
+
   module.exports = blogsRouter
