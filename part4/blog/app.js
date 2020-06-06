d1a49a72cd763f34b26ed39d1a9cfa901ae3ca48
Oliver Lang <lang.oliver@gmail.com>
2020-06-06 11:06:48 +0200

    4.20*: bloglist expansion, step8

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -26,10 +26,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   app.use(express.json())
 
   app.use(middleware.requestLogger)
+  app.use(middleware.tokenExtractor)
+
   app.use('/api/users', usersRouter)
   app.use('/api/blogs', blogsRouter)
   app.use('/api/login', loginRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
   
--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,13 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -20,7 +14,7 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response, next) => {
     let blog = new Blog(request.body)
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
