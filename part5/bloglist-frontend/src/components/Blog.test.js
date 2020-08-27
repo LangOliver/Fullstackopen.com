c209f58d1e303311d140e3d56c64b36f4894820a
Oliver Lang <lang.oliver@gmail.com>
2020-08-27 14:23:02 +0200

    5.16*: Blog list tests, step4 - part

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
+import BlogForm from './BlogForm'
 
 test('renders blog basic content', () => {
   const blog = {
@@ -82,4 +83,29 @@ test('when the like button is clicked twice, the event handler the component rec
   fireEvent.click(likeButton)
   expect(updateBlog.mock.calls).toHaveLength(2)
   component.debug()
+})
+
+test('blog form calls the event handler it receives as props', () => {
+  const blog = {
+    title: 'Most awesome post ever',
+    author:'Oliver Lang',
+    likes: 12,
+    url: 'http://www.ich.ch'
+  }
+
+  const createBlog = () => {
+
+  }
+
+  const component = render(
+    <BlogForm createBlog={createBlog}/>)
+  component.debug()
+  const form = component.container.querySelector('form')
+  fireEvent.submit(form)
+
+  const author = component.container.querySelector('#author')
+  const title = component.container.querySelector('#title')
+  expect(author.onChange).toBeDefined
+  expect(title.onChange).toBeDefined
+
 })
--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,61 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const BlogForm = ({ createBlog}) => {
-    const [title, setTitle] = useState('') 
-    const [author, setAuthor] = useState('') 
-    const [url, setURL] = useState('') 
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setURL] = useState('')
 
-    
-    const addBlog = (event) => {
-        event.preventDefault()
-        console.log('addBlogg function called, handle the object creation')
-        createBlog({
-            author: author,
-            title: title,
-            url: url
-        })
-        setTitle('')
-        setAuthor('')
-        setURL('')
-    }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    console.log('addBlogg function called, handle the object creation')
+    createBlog({
+      author: author,
+      title: title,
+      url: url
+    })
+    setTitle('')
+    setAuthor('')
+    setURL('')
+  }
 
   return (
     <div>
       <h2>Create a new blog</h2>
       <form onSubmit={addBlog}>
-      <div>
+        <div>
          title
-           <input
-           type="text"
-           value={title}
-           name="title"
-           onChange={({ target }) => {
-               setTitle(target.value)
+          <input
+            type="text"
+            id='title'
+            value={title}
+            name="title"
+            onChange={({ target }) => {
+              setTitle(target.value)
             }
             }
-     
-        />
+
+          />
         </div>
         <div>
           author
-            <input
+          <input
             type="text"
+            id='author'
             value={author}
             name="author"
             onChange={({ target }) => {
-                setAuthor(target.value)
+              setAuthor(target.value)
             }
             }
           />
         </div>
         <div>
           url
-            <input
+          <input
             type="text"
+            id='url'
             value={url}
             name="url"
             onChange={({ target }) => {
-                setURL(target.value)
+              setURL(target.value)
             }
             }
           />
