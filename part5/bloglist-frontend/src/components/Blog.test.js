e531df63cc0481e8421b3da0adf11fa13ae23533
Oliver Lang <lang.oliver@gmail.com>
2020-08-27 11:07:31 +0200

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog basic content', () => {
@@ -35,7 +35,27 @@ test('renders only the blog\'s basic content', () => {
   expect(component.container).not.toHaveTextContent('12')
   expect(component.container).not.toHaveTextContent('http://www.ich.ch')
 
+})
 
+test('renders the blog\s url and likes when the show button is clicked', () => {
+  const blog = {
+    title: 'Most awesome post ever',
+    author:'Oliver Lang',
+    likes: 12,
+    url: 'http://www.ich.ch'
+  }
+
+  const component = render(
+    <Blog blog={blog} />)
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const div = component.container.querySelector('.blogDetails')
+  expect(div).toHaveTextContent('12')
+  expect(div).toHaveTextContent('http://www.ich.ch')
+
+  component.debug()
 
 
 
